d7abbe53245365463aff38beb78346b6fc86070e
Alejandro Jiménez <aljrico@gmail.com>
2018-07-19 18:35:56 +0200

    compatibility file

---
+++ R/compatibility.R
@@ -0,0 +1,17 @@
+#' Original 'Harry Potter' colour map
+#'
+#' A dataset containing the averaged RGB values of each frame from every movie of the Harry Potter saga.
+#'
+#'  Sources: \url{http://movie-colors.com/hp/}..
+#'
+#'
+#' @format A data frame with 4 variables:
+#' \itemize{
+#'   \item V1: Red Value
+#'   \item V2: Green Value
+#'   \item V3: Blue Value
+#'   \item movie: The movie from where the colourmap is extracted
+#' }
+#'
+#' @export
+viridis.map <- harrypotterLite:::hp.map
