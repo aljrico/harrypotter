b8c14a56ecbfb626133e0faa186ea26ae89ae3be
Alejandro Jiménez <aljrico@gmail.com>
2018-07-19 18:16:00 +0200

    following Hadley instructions

--- R/hp_palette.R
+++ R/hp_palette.R
@@ -96,7 +96,7 @@ hp <- function(n, alpha = 1, begin = 0, end = 1, direction = 1, movie = 1) {
 	if(movie == 7.1) movie <- 7
 	if(movie == 7.2) movie <- 8
 
-	hp.map <- harrypotter::hp.map
+	hp.map <- harrypotter:::hp.map
 	colnames(hp.map) <- c("R", "G", "B", "movie")
 
 	map <- hp.map[hp.map$movie == movie, ]
@@ -130,7 +130,7 @@ hpMap <- function(n = 256, alpha = 1, begin = 0, end = 1, direction = 1, movie =
 	}
 
 
-	hp.map <- harrypotter::hp.map
+	hp.map <- harrypotter:::hp.map
 	colnames(hp.map) <- c("R", "G", "B", "movie")
 
 	if(movie == 7.1) movie <- 7
