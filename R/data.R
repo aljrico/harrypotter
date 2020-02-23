86c503a2d0b069be2322f4f1397425363a96b1e4
aljrico <aljrico@gmail.com>
2020-02-23 19:32:08 +0000

    documentation properly placed and error fixed

---
+++ R/data.R
@@ -0,0 +1,22 @@
+#' Original 'Harry Potter' colour map
+#'
+#' A dataset containing some colour palettes inspired on the Harry Potter Universe
+#'
+#'
+#'@format A data frame containing all the colours used in the palette:
+#'\itemize{
+#'   \item V1: Red value
+#'   \item V2: Green value
+#'   \item V3: Blue value
+#'   \item option: It is intended to be a general option for choosing the specific colour palette.
+#'}
+"hp.map"
+
+#' Available Palettes.
+#'
+#' This list contains all the available palettes in the 'harrypotter' package.
+#'
+#'@format A list containing all palettes color codes.
+#'
+#' @export
+"hp_palettes"
