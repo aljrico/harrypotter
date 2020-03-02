cc0f03755f74598d9c1de2c3c187730db4ea3e38
aljrico <aljrico@gmail.com>
2020-03-02 21:12:34 +0000

    test length

--- tests/testthat/test-length.R
+++ tests/testthat/test-length.R
@@ -1,5 +1,8 @@
 context("test-length")
 
 test_that("hp returns correct number of colors", {
+	expect_equal(length(hp(1)), 1)
 	expect_equal(length(hp(10)), 10)
+	expect_equal(length(hp(1e2)), 1e2)
+	expect_equal(length(hp(1e3)), 1e3)
 })
